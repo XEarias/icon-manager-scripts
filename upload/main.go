4cc55bcf7284debd871f91727f65fbae23e56752
Xavier Arias <xarias13@gmail.com>
2018-04-12 14:22:23 -0300

    .

--- upload/main.go
+++ upload/main.go
@@ -33,7 +33,7 @@ type DB struct {
 var DBConfig = DB{
 	ip:       "127.0.0.1",
 	port:     "3306",
-	name:     "disenadorlogodb",
+	name:     "testdisenadorlogodb",
 	user:     "logoPro",
 	password: "&rJ-fZ:1uZ24",
 }
